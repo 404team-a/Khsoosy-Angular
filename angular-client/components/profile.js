861842df294da1f956f4b3e8209253e29940a5df
yousefalshabahm <yousefalshabah@hotmail.com>
2019-05-28 02:06:12 +0300

    im going to sleep bye

--- angular-client/components/profile.js
+++ angular-client/components/profile.js
@@ -1,5 +1,14 @@
 angular.module('app')
+ .controller('appCtrl',function($scope){
+    
 
+
+    this.changeCV = () =>{
+
+}
+
+ })
 .component('Profile' ,{
+
     templateUrl : "angular-client/templates/profile.html" 
 })
