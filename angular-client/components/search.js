5daf4f5d3f6eff2e07a36b3251abc2b86ef4d9b3
CanaanGM <reakak1@gmail.com>
2019-05-28 02:30:15 +0300

    search done but not sure it's working

--- angular-client/components/search.js
+++ angular-client/components/search.js
@@ -1,5 +1,28 @@
-(function(angular) {
-    angular.module('search', []).controller('searchCtrl', function searchCtrl() {
-        // functionality goes here !
-    });
-})(window.angular);
+import { app } from "firebase";
+
+// (function(angular) {
+//     angular.module('search', []).controller('searchCtrl', function searchCtrl($scope) {
+//         // functionality goes here !
+//         var searchBox =  $scope.searchBox
+//         this.fun=function (params) {
+//         }
+//     });
+
+//     angular.module('search', []).c
+// })(window.angular);
+
+var subjectsName = angular.module('searchForm', []);
+var subjectLevel = angular.module("searchForm", []);
+var location = angular.module("searchBox", []);
+
+app.controller('searchCtrl', ($scope) => {
+    $scope.subjectsName 
+});
+
+app.controller('searchCtrl', ($scope) => {
+    $scope.subjectLevel
+});
+
+app.controller('searchCtrl', ($scope) => {
+    $scope.location
+});
