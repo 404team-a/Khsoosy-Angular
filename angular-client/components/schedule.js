e2d423bacf82d3299432d841cf23aba25858f802
Mfelokour <Mfelokour@gmail.com>
2019-05-27 18:30:51 +0300

    schedule-

---
+++ angular-client/components/schedule.js
@@ -0,0 +1,7 @@
+angular
+  .module("app")
+  .controller("fnSchedule", function(data) {})
+  .component("schedule", {
+    controller: "fnSchedule",
+    templateUrl: "angular-client/templates/schedule.html"
+  });
