1262785643350b1976144f4c60c055c65775789c
Mfelokour <Mfelokour@gmail.com>
2019-05-27 15:45:24 +0300

    first commit

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const db = require('./database/db');
+const express = require("express");
+const bodyParser = require("body-parser");
+const db = require("./database/db");
 const app = express();
-const khsoosiRouter = require('./router');
+const khsoosiRouter = require("./router");
 
 const port = process.env.PORT || 4000;
 
@@ -10,14 +10,17 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-	next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 
-app.use('', khsoosiRouter);
-app.use(express.static(__dirname + '/../client/dist'));
+app.use("", khsoosiRouter);
+app.use(express.static(__dirname + "/../client/dist"));
 
 app.listen(port, function() {
-	console.log('listening on port !', port);
+  console.log("listening on port !", port);
 });
