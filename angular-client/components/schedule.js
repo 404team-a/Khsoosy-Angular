a57a9d441caeca6e5512e21c0141e603db6d94ce
Mfelokour <Mfelokour@gmail.com>
2019-05-27 23:40:02 +0300

    any

--- angular-client/components/schedule.js
+++ angular-client/components/schedule.js
@@ -1,12 +1,49 @@
 angular
   .module("app")
-  .controller("fnSchedule", ['$scope', function($scope) {
-     $scope.schedules = [ ...temp, { day, startHour, endHour } ];
-     $scope.
-  }])
-  
-  
+  .controller("fnSchedule", [
+    "$scope",
+    function($scope) {
+      $scope.day = "Sunday";
+      $scope.startHour = "";
+      $scope.endHour = "";
+      $scope.message = "";
+      $scope.schedules = [];
+
+      $scope.addSchedule = () => {
+        const { day, startHour, endHour } = this.$scope;
+        const temp = this.$scope.schedules;
+        if (startHour >= endHour || _.pluck(this.props.schedules, 'day').includes(day)) {
+          // console.log(startHour, endHour);
+          this.setState({
+            startHour: '',
+            endHour: '',
+            message: 'Check your Time!'
+          });
+        } else {
+          this.setState (
+            {
+              schedules: [ ...temp, { day, startHour, endHour } ],
+              message: '',
+              startHour: '',
+              endHour: ''
+            },
+            () => {
+              this.props.changeSchedules(this.state.schedules);
+            }
+          );
+        }
+      }
+
+
+
+
+      } 
+
+    }
+  ])
+
   .component("schedule", {
     controller: "fnSchedule",
     templateUrl: "angular-client/templates/schedule.html"
   });
+
