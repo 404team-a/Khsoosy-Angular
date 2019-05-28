3e27f8f2882ec24e1bfd71683d332466c144e4e4
BasmaAltayeh <basma.altayeh@gmail.com>
2019-05-28 13:16:20 +0300

    rate message

--- angular-client/components/Rating.js
+++ angular-client/components/Rating.js
@@ -1,9 +1,39 @@
 angular.module('app')
+.controller('rateController'['$scope',function($scope){
+  $scope.ratingText='';
+  $scope.rate='';
+  $scope.rateMessage='Thank you for your feedback!'
+
+  $scope.rating=function(){
+    var body = {
+      rating :$scope.ratingText,
+      tera:$scope.rate,
+      teacherId:this.teachrId,
+      stdId:this.stdId
+    }
+    $http({
+			method:'post',
+			url:'/login',
+			data:JSON.stringify(body),
+		  headers: {'Content-Type': "application/json; charset = utf-8"}
+		}).then(function(response){
+            console.log('success')
+            
+		}).catch(function(){
+			console.log('big error')
+		})
+  }
+
+}])
+
+
+
 .component('Rating',{
   bindings: {
-      items: '<',
+    teacherId:'<',
+    stdId:'<'
     },
-    controller: function() {},
+    controller: 'rateController',
     templateUrl: '/templates/Rating.html'
 
 })
