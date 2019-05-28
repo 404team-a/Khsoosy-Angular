ffd1a76db6fabab348e253b5b576f5dadec43f72
Mfelokour <Mfelokour@gmail.com>
2019-05-28 17:54:23 +0300

    sch

--- angular-client/components/schedule.js
+++ angular-client/components/schedule.js
@@ -1,29 +1,43 @@
-angular
-  .module("app")
-  .controller("fnSchedule", [
-    "$scope",
-    function($scope) {
-      $scope.day = "Sunday";
+angular.module("app", []).controller("ScheduleController", function($scope) {
+  $scope.schedules = [];
+  $scope.startHour = "";
+  $scope.add = function() {
+    if ($scope.startHour) {
+      $scope.schedules.push(this.startHour);
       $scope.startHour = "";
-      $scope.endHour = "";
-      $scope.message = "";
-      $scope.schedules = [];
-      $scope.entry = false;
-
-      $scope.addSchedule = () => {
-        const { day, startHour, endHour } = this.$scope;
-        const temp = this.$scope.schedules;
-        if (
-          startHour >= endHour ||
-          _.pluck(this.props.schedules, "day").includes(day)
-        ) {
-          // console.log(startHour, endHour);
-        }
-      };
     }
-  ])
+    $scope.delete = function() {
+      $scope.schedules.splice(this.$index, 1);
+    };
+  };
+});
 
-  .component("schedule", {
-    controller: "fnSchedule",
-    templateUrl: "angular-client/templates/schedule.html"
-  });
+// angular
+//   .module("app")
+//   .controller("ScheduleController", [
+//     "$scope",
+//     function($scope) {
+//       $scope.day = "Sunday";
+//       $scope.startHour = "";
+//       $scope.endHour = "";
+//       $scope.message = "";
+//       $scope.schedules = [];
+//       $scope.entry = false;
+
+//       $scope.addSchedule = () => {
+//         const { day, startHour, endHour } = this.$scope;
+//         const temp = this.$scope.schedules;
+//         if (
+//           startHour >= endHour ||
+//           _.pluck(this.props.schedules, "day").includes(day)
+//         ) {
+//           // console.log(startHour, endHour);
+//         }
+//       };
+//     }
+//   ])
+
+//   .component("schedule", {
+//     controller: "fnSchedule",
+//     templateUrl: "angular-client/templates/schedule.html"
+//   });
