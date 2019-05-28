5561820de712dd4799a3d108efcca6a0f8f8eae7
BasmaAltayeh <basma.altayeh@gmail.com>
2019-05-28 14:43:15 +0300

    start with Sign up

---
+++ angular-client/components/SignUp.js
@@ -0,0 +1,12 @@
+angular.module('app')
+.controller('rateController'['$scope',function($scope){
+
+}])
+
+.component('Rating',{
+    bindings: {
+      },
+      controller: 'signUpController',
+      templateUrl: '/templates/Rating.html'
+  
+  })
