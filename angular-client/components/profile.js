84cd062240acb60526a13dd1223672d5cf1ae0c5
yousefalshabahm <yousefalshabah@hotmail.com>
2019-05-28 21:25:44 +0300

    the mount and showTeacher functions is readdy

--- angular-client/components/profile.js
+++ angular-client/components/profile.js
@@ -1,14 +1,56 @@
 angular.module('app')
- .controller('appCtrl',function($scope){
+ .controller('ProfileCtrl',function($scope,$http){
     $scope.cv=''
     this.updatedMsg = "thanks a lote "
 
-    this.changeCV = () =>{
+   
+    this.componentDidMount = () => {
+        const id = this.current_teacherId;
+        this.current_teacherId = id
+        this.showTeacherInfo(id)
+    }
+    this.showTeacherInfo = (id) => {
+        return $http(`/teacherProfile/${id}`, {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+				Accept: 'application/json'
+			}
+		})
+			.then((response) => (response = response.json()))
+			.then((data) => {
+				// console.log(data);
+				
+					$scope.userName = data.name
+					$scope.cvFileUrl = data.cvFile
+					$scope.imgUrl= data.img
+					$scope.email = data.email
+					$scope.phone = data.phone
+					$scope.location = data.location
+					$scope.summary = data.summary
+					$scope.schedules = data.schedules
+					$scope.ratings = data.ratings
+					$scope.subjectName = data.subjects[0].name
+					$scope.subjectLevel = data.subjects[0].level
+				
+			})
+			.catch((err) => console.log(err));
+    }
+    this.updateInfo = () => {
 
-}
+    }
+    this.changeCV = (cvFileUrl) => {
+
+    }
+    this.changeImg = (imgUrl) => {
+
+    }
+    this.changeSchedules = (schedules) => {
+
+    }
 
  })
 .component('Profile' ,{
-
+    controller : "ProfileCtrl",
     templateUrl : "angular-client/templates/profile.html" 
 })
