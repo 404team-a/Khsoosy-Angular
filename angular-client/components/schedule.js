5bbd1949ec14393aa5454e665ae1c62455aca32c
Mfelokour <Mfelokour@gmail.com>
2019-05-29 21:03:13 +0300

    schedule component complete

--- angular-client/components/schedule.js
+++ angular-client/components/schedule.js
@@ -1,46 +1,35 @@
 angular
-  .module("app", [])
+  .module("app")
 
   .controller("ScheduleController", function($scope) {
     $scope.schedules = [];
-    $scope.startHour = "";
+    //rowArr[0] = day
+    //rowArr[1] = startHour
+    //rowArr[2] = endHour
+    $scope.rowArr = ["Sunday", "", ""];
+    $scope.isVisible = false;
     $scope.add = function() {
-      if ($scope.startHour) {
-        $scope.schedules.push(this.startHour);
-        $scope.startHour = "";
+      if ($scope.rowArr[0] && $scope.rowArr[1] && $scope.rowArr[2]) {
+        $scope.schedules.push($scope.rowArr);
+        $scope.rowArr = [];
+        // console.log($scope.schedules);
       }
+      $scope.showHide = function() {
+        $scope.isVisible = $scope.isVisible = true;
+      };
       $scope.delete = function() {
         $scope.schedules.splice(this.$index, 1);
+        // console.log($scope.schedules);
+      };
+      $scope.hide = function() {
+        if ($scope.schedules.length === 0) {
+          $scope.isVisible = $scope.isVisible = false;
+        }
       };
     };
   })
 
   .component("schedule", {
     controller: "ScheduleController",
-    templateUrl: "angular-client/templates/schedule.html"
+    templateUrl: "/templates/schedule.html"
   });
-
-// angular
-//   .module("app")
-//   .controller("ScheduleController", [
-//     "$scope",
-//     function($scope) {
-//       $scope.day = "Sunday";
-//       $scope.startHour = "";
-//       $scope.endHour = "";
-//       $scope.message = "";
-//       $scope.schedules = [];
-//       $scope.entry = false;
-
-//       $scope.addSchedule = () => {
-//         const { day, startHour, endHour } = this.$scope;
-//         const temp = this.$scope.schedules;
-//         if (
-//           startHour >= endHour ||
-//           _.pluck(this.props.schedules, "day").includes(day)
-//         ) {
-//           // console.log(startHour, endHour);
-//         }
-//       };
-//     }
-//   ])
