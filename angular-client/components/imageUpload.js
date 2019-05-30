d85e7490460de240ef8f50db58dc145ce755d4b7
Mfelokour <Mfelokour@gmail.com>
2019-05-30 15:06:34 +0300

    sort of done with the ImageUpload Component

--- angular-client/components/imageUpload.js
+++ angular-client/components/imageUpload.js
@@ -7,15 +7,54 @@ angular
       files: [],
       image: null,
       imgUrl: "",
-      progress: 0
+      progress: 0,
+      appearImg = false,
+      showProgress = false,
     };
 
-    $scope.imgUpload() {
-      $scope.imgUrl = $scope.imgUrl
+    $scope.imgUpload = function () {
+      $scope.imgUrl = imgUrl;
+      if ($scope.image) {
+        $scope.showImg = () => {
+          appearImg = true;
+        } 
+      }
+      uploadTask.on(
+        "Uploading Progress",
+        snapshot => {
+          //progress function ....
+          const progress = Math.round(
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+          );
+          this.setState({ progress });
+        },
+        error => {
+          // error function ....
+          console.log(error);
+        },
+        () => {
+          // complete function ....
+          storage
+            .ref("images")
+            .child(image.name)
+            .getDownloadURL()
+            .then(imgUrl => {
+              setTimeout(() => {
+                $scope.showProgress({ imgUrl, progress: 0 }, () =>
+                  changeImg($scope.imgUrl)
+                );
+              }, 2000);
+            });
+        }
+      );
     }
   })
 
   .component("imageupload", {
+    bindings: {
+      imgUrl:'<',
+      changeImg: '<'
+    },
     controller: "ImageUploadController",
     templateUrl: "/templates/schedule.html"
   });
